83765328fd79f0ab6af80aeeb5d1f080ab90485d
Your Name <you@example.com>
2019-04-08 14:34:20 +0530

    Add tests for DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -5,118 +5,115 @@ const _ = require('lodash');
 const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
-    email: {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
       type: String,
-      required: true,
-      trim: true,
-      minlength: 1,
-      unique: true,
-      validate: {
-        validator: validator.isEmail,
-        message: '{VALUE} is not a valid email'
-      }
+      required: true
     },
-    password: {
+    token: {
       type: String,
-      require: true,
-      minlength: 6
-    },
-    tokens: [{
-      access: {
-        type: String,
-        required: true
-      },
-      token: {
-        type: String,
-        required: true
-      }
-    }]
-  });
+      required: true
+    }
+  }]
+});
 
-UserSchema.methods.toJson = function () {
+UserSchema.methods.toJSON = function () {
   var user = this;
   var userObject = user.toObject();
-  return _.pick(userObject, ['_id','email']);
+
+  return _.pick(userObject, ['_id', 'email']);
 };
 
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexaString(),access},'abc123').toString();
-  user.tokens = user.tokens.concat([{access,token}]);
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
- return user.save().then(() => {
+  user.tokens.push({access, token});
+
+  return user.save().then(() => {
     return token;
-  })
+  });
 };
 
 UserSchema.methods.removeToken = function (token) {
   var user = this;
 
- return  user.update({
+  return user.update({
     $pull: {
-      tokens: {
-        token: {token}
-      }
+      tokens: {token}
     }
-  })
-}
+  });
+};
 
 UserSchema.statics.findByToken = function (token) {
- var User = this;
- var decoded;
+  var User = this;
+  var decoded;
 
-try {
- 
-  decoded = jwt.verify(token, 'abc123');
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
 
-} catch (e) {
-   return Promise.reject( );
-}
-
-return User.findOne({
-  _id: decoded._id,
-  'tokens.token': token,
-  'tokens.access': 'auth'
-})
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
 };
 
 UserSchema.statics.findByCredentials = function (email, password) {
   var User = this;
 
-  User.findOne({email}).then((user) => {
-    if (!user){
+  return User.findOne({email}).then((user) => {
+    if (!user) {
       return Promise.reject();
     }
 
     return new Promise((resolve, reject) => {
-    bcrypt.compare(password, user.password, (err, res) => {
-       if(res) {
-         resolve(user);
-       } else {
-         reject();
-       }
-    })
+      // Use bcrypt.compare to compare password and user.password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
     });
-  })
-}
+  });
+};
 
-UserSchema.pre('save', function(nest){
+UserSchema.pre('save', function (next) {
   var user = this;
 
- if (user.isModified('password')) {
-   bcrypt.genSalt (10, (err,salt) => {
-     bcrypt.hash(user.password, salt, (err, hash) => {
-       user.password = hash;
-       next();
-     })
-   })
-
-
- } else {
-   next();
- }
-})
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model('User', UserSchema);
 
--- server/server.js
+++ server/server.js
@@ -103,42 +103,37 @@ app.post('/users', (req, res) => {
   var user = new User(body);
 
   user.save().then(() => {
-   return user.generateAuthToken();
-    // res.send(user);
+    return user.generateAuthToken();
   }).then((token) => {
-   res.head('x-auth',token).send(user);
+    res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
   })
 });
 
-app.get('/users/me', authenticate, (req,res) => {
+app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
-// POST /users/login {email , password}
-
-app.post('/users/login', (req,res) => {
-  var body =_.pick(req.body, ['email', 'password']);
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
 
   User.findByCredentials(body.email, body.password).then((user) => {
-    return user.generateAuthToken().then ((token) => {
-      res.head('x-auth',token).send(user);
-   });
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
   }).catch((e) => {
     res.status(400).send();
   });
-
-  
-})
+});
 
 app.delete('/users/me/token', authenticate, (req, res) => {
-   req.user.removeToken(req.token).then (() => {
-   res.status(200).send();
-   }, () => {
-     res.status(400).send();
-   })
-})
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
